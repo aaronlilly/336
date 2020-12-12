1658e66e01adf6aaca66c98da188c3a58f4d82f0
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-12-12 13:05:08 -0500

    mayb

--- PokemonBeyond/scripts/checkbinssite.js
+++ PokemonBeyond/scripts/checkbinssite.js
@@ -2,11 +2,14 @@ var col = '5fd3a4b982e9306ae60054c3';
 var label = [];
 var upLabel = [];
 
+
  $(document).ready(function ()  
            {
 			   $.ajax
                          ({
                     method: "GET",
+                     complete: function(r){
+       //iD.Push(r.responseText);
 
 					beforeSend: function (xhr) {
   					 xhr.setRequestHeader("secret-key", mySecretKey);
@@ -24,17 +27,12 @@ var upLabel = [];
 
 
 function next(){
-//console.log(label[0].bNs[0].trainer)
 upLabel.push(label[0]);
-//console.log(upLabel[0]);
-//console.log(upLabel[0].bns[0].tname);
-
 }
 
 
 $(document).ready(function(){
 $("#sendMeI").click(function(){
-
 checkIt()
 
 
@@ -49,25 +47,29 @@ alert("nothing happened");
 }
 
 
+
+
+
 function IsDifferent(){
-//upLabel[0].bNs.push({"trainer":"789","bn":"aaa"});
-upLabel[0].bns.push({"tname": "`345","bin": "b"});
-IsDifferenter();
-}
-
-
-function IsDifferenter(){
  let req = new XMLHttpRequest();
 
 req.onreadystatechange = () => {
   if (req.readyState == XMLHttpRequest.DONE) {
   console.log(req.responseText);
+
+var tNam = $('#igname').val();
+
+upLabel[0].bns.push(  {"tname": tNam,"bin": req.responseText});
+
   }
 };
 
  req.open("PUT", "https://api.jsonbin.io/b/" +col , true);
 req.setRequestHeader("secret-key", mySecretKey);
 req.setRequestHeader("Content-Type", "application/json");
+
+
+
 
 data2 = JSON.stringify(upLabel);
 req.send(data2);
