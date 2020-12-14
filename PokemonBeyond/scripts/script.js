96cfc9f491503d788754e9c32420e01c2bc620a7
Aaron Lilly <aaronlilly84@gmail.com>
2020-12-13 22:45:54 -0500

    need work on the splice

--- PokemonBeyond/scripts/script.js
+++ PokemonBeyond/scripts/script.js
@@ -1,42 +1,3 @@
-
-function psh(){
-x=wantP.length;
-
-if(x==1){
-pone(wantP[0].name,wantP[0].type);
-
-}else if(x>1){
-ptwo();
-}
-}
-
-function pone(x,y){
-var start = '{"employees": {"atU": { "';
-var mid = '": { "pokemonWant": [{ "name": "';
-var miB = '", "type": "'
-var miB2 = '"}';
-var BotM = '],"xan": "';
-var nD = '" } }}}'
-
-    var tn = uP[0];
-  var zw = dowP[0];
-  //tn.toUpperCase()
-
-alert(start + tn.toUpperCase() +mid + x + miB + y + miB2+ BotM + zw + nD);
-}
-
-function ptwo(){
-  var tn = uP[0];
-  var zw = dowP[0];
-
-var text = "";
-for(i= 0; i < wantP.length;){
-text += wantP[i].name;
-i++;
-}
-alert( tn + zw.toUpperCase() +text);
-}
-
 $(document).ready(function(){
   for (i = 0; i < pokemonlist.length; i++) {
   $('#btnns').append('<button type="button" name ="bt" id="'+pokemonlist[i].name+ '\" class="btn btn-' +bsl[i] +' "style="margin:3px">'+ pokemonlist[i].name + '</button>')};
@@ -56,13 +17,15 @@ if ($("#" + currentId +"box").prop('checked')) {
    $("#" + currentId +"span").html("");
     $("#" + currentId+"box").prop('checked',false)
     
-    for (x = 0; x < wantP.length; x++) {
+    for (x = 0; x < wantP.length+1; x++) {
      
       if(wantP[x] == currentId){
         wantP.splice(x,1);
-        wantPtype.splice(x,1);
+       
       }
     }
+
+
   }
   //if not checked
    else{
@@ -70,10 +33,10 @@ if ($("#" + currentId +"box").prop('checked')) {
     $("#append").append("<span id ='" + currentId + "span'></span>"); 
     $("#" + currentId +"span").append(currentId + "\t"); 
     $("#" + currentId +"box").prop('checked',true);
-  wantP.push(pName);
-  wantPtype.push(pType);
+  wantP.push({"Name" : pName, "Type" : pType});
+  // {"tname": tNam,"bin": bin}
   }
-
+//
 });
 }
 });
@@ -85,25 +48,60 @@ $(document).ready(function(){
 $("#sendMe").click(function(){
 
 
-uP.push($('#igname').val());
-dowP.push($('#xpx').val());
-psh();
-udt();
+// uP.push($('#igname').val());
+// dowP.push($('#xpx').val());
+
 });
 });
 
 
 //checking trainer name if match a trainer name that exists
-function udt(){
-for (i = 0; i < label[0].bNs.length; i++) {
-//console.log(label[i].bNs[i].trainer)
+// function udt(){
+// for (i = 0; i < label[0].bNs.length; i++) {
+// //console.log(label[i].bNs[i].trainer)
 
-if (uP[0] == label[0].bNs[i].trainer)
-binuPdt();
+// if (uP[0] == label[0].bNs[i].trainer)
+// binuPdt();
 
-}
-}
+// }
+// }
 
-function binuPdt(){
+// ========
 
-};
+// function psh(){
+// x=wantP.length;
+
+// if(x==1){
+// pone(wantP[0].name,wantP[0].type);
+
+// }else if(x>1){
+// ptwo();
+// }
+// }
+
+// function pone(x,y){
+// var start = '{"employees": {"atU": { "';
+// var mid = '": { "pokemonWant": [{ "name": "';
+// var miB = '", "type": "'
+// var miB2 = '"}';
+// var BotM = '],"xan": "';
+// var nD = '" } }}}'
+
+//     var tn = uP[0];
+//   var zw = dowP[0];
+//   //tn.toUpperCase()
+
+// alert(start + tn.toUpperCase() +mid + x + miB + y + miB2+ BotM + zw + nD);
+// }
+
+// function ptwo(){
+//   var tn = uP[0];
+//   var zw = dowP[0];
+
+// var text = "";
+// for(i= 0; i < wantP.length;){
+// text += wantP[i].name;
+// i++;
+// }
+// alert( tn + zw.toUpperCase() +text);
+// }
