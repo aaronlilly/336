05582761c4eb74b238a92827422231d94762c913
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-12-30 13:09:57 -0500

    Update ptcOneScript.js

--- pb/scripts/ptcOneScript.js
+++ pb/scripts/ptcOneScript.js
@@ -1,4 +1,20 @@
- $(document).ready(function ()  
-    {
-		
-});
+//$(document).ready(function() {
+
+//alert('hit');
+//});
+
+
+
+   $(document).ready(function () { 
+ var COOKI= Cookies.get('name');
+     //alert(COOKI);
+       if (COOKI != "") {
+         cNBlank(COOKI);
+      }
+         });
+
+function cNBlank(COOKI){
+    if(COOKI !== undefined){
+    alert("Welcome again " + COOKI);
+    }
+  }
