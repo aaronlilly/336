eefa04ff1a677c0a9b34d762c5f7d3e49b9ee9d9
Aaron Lilly <aaronlilly84@gmail.com>
2021-01-08 08:27:08 -0500

    d

--- pb/scripts/ptcOneScript.js
+++ pb/scripts/ptcOneScript.js
@@ -134,3 +134,24 @@ for(var i=0; i <  pokemonNameArray.length; i++)
                         $("#xboxs").append('<input type="checkbox" id="' + pokemonNameArray[i] +'box">');
                       }
 });
+
+
+//////////
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": 0,
+  "extendedTimeOut": 0,
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut",
+  "tapToDismiss": false
+}
