e91b1bd03abac6b722be67d3f1183577f677a83a
Aaron Lilly <aaronlilly84@gmail.com>
2021-01-10 07:30:32 -0500

    html try

--- pb/scripts/ptcOneScript.js
+++ pb/scripts/ptcOneScript.js
@@ -308,18 +308,18 @@ function getHaveWant(){
                           $('#wantPaste').html("");
 
                             for(var i = 0; i <dataX[0].AaronAwezom.Have.length; i++) {
-                      $('#havePaste').append("<img src='" + dataX[0].AaronAwezom.Have[i].imaj +"'>");
-                    }
+                      $('#havePaste').html("<img src='" + dataX[0].AaronAwezom.Have[i].imaj +"'>");
+                    }rePainter();
                     //console.log(dataX[0].AaronAwezom.Have[0].Name)
 
                       
 
                         for(var i = 0; i <dataX[0].AaronAwezom.Have.length; i++) {
-                       $('#wantPaste').append("<img src='" + dataX[0].AaronAwezom.Want[i].imaj +"'>");
-                    }
+                       $('#wantPaste').html("<img src='" + dataX[0].AaronAwezom.Want[i].imaj +"'>");
+                    }rePainter1();
                     
     });
-                  
+
 }
 }
 
@@ -330,6 +330,10 @@ function rePainter(){
 $('#wantPaste').hide();
 $('#wantPaste').get(0).offsetHeight;
 $('#wantPaste').show();
+
+};
+
+function rePainter1(){ 
 $('#havePaste').hide();
 $('#havePaste').get(0).offsetHeight;
 $('#havePaste').show();
