097ad46985e0aea8c0fadaf7915f98aa50a07064
Aaron Lilly <aaronlilly84@gmail.com>
2021-01-10 07:18:26 -0500

    two not one

--- pb/scripts/ptcOneScript.js
+++ pb/scripts/ptcOneScript.js
@@ -309,25 +309,31 @@ function getHaveWant(){
 
                             for(var i = 0; i <dataX[0].AaronAwezom.Have.length; i++) {
                       $('#havePaste').append("<img src='" + dataX[0].AaronAwezom.Have[i].imaj +"'>");
-                    }
+                    }function rePainter();
                     //console.log(dataX[0].AaronAwezom.Have[0].Name)
 
                       
 
                         for(var i = 0; i <dataX[0].AaronAwezom.Have.length; i++) {
                        $('#wantPaste').append("<img src='" + dataX[0].AaronAwezom.Want[i].imaj +"'>");
-                    }
-                    function rePainter();
+                    }function rePainter1();
+                    
     });
+
 }
 }
 
 // label.push(data);
 // ;
 
-function rePainter(){ $('#wantPaste').hide();
+function rePainter(){ 
+$('#wantPaste').hide();
 $('#wantPaste').get(0).offsetHeight;
 $('#wantPaste').show();
+
+};
+
+function rePainter1(){ 
 $('#havePaste').hide();
 $('#havePaste').get(0).offsetHeight;
 $('#havePaste').show();
