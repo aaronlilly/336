5e8f975ae49a7f909ddae49171c0c7cafe1e39d5
Aaron Lilly <aaronlilly84@gmail.com>
2021-02-18 20:37:27 -0500

    want works
    df

--- pb/scripts/ptcOneScript.js
+++ pb/scripts/ptcOneScript.js
@@ -1300,53 +1300,47 @@ $(document).ready(function () {
                            '</figcaption>'+'</figure>'+'</div></div>'); 
 
   $('#'+ data.PokemonTradingCenterGalar[a].Name + "galx").click(function(){
-           
-            
-
-
-
-   var gDex = data.PokemonTradingCenterGalar[a].Dex;
-  var gName = data.PokemonTradingCenterGalar[a].Name;
-  var gReg = data.PokemonTradingCenterGalar[a].Region;
-  var gType1 = data.PokemonTradingCenterGalar[a].Type1;
-  var gType2 = data.PokemonTradingCenterGalar[a].Type2;
-  var gImj = data.PokemonTradingCenterGalar[a].imaj;
-  var gdC = data.PokemonTradingCenterGalar[a].datecaught;
-  var gNotes = data.PokemonTradingCenterGalar[a].notes;
-  var gPur = data.PokemonTradingCenterGalar[a].purified;
-  var gShiny = data.PokemonTradingCenterGalar[a].shiny;
-                              var curre = $(this).attr('id');
+    var gDex = data.PokemonTradingCenterGalar[a].Dex; var gName = data.PokemonTradingCenterGalar[a].Name;   var gReg = data.PokemonTradingCenterGalar[a].Region;   var gType1 = data.PokemonTradingCenterGalar[a].Type1;   var gType2 = data.PokemonTradingCenterGalar[a].Type2;   var gImj = data.PokemonTradingCenterGalar[a].imaj;   var gdC = data.PokemonTradingCenterGalar[a].datecaught;   var gNotes = data.PokemonTradingCenterGalar[a].notes;   var gPur = data.PokemonTradingCenterGalar[a].purified;   var gShiny = data.PokemonTradingCenterGalar[a].shiny; var curre = $(this).attr('id');
                               
-                              
-
               if($("#" + curre + "wanbox").prop('checked'))
               {
-    
-     for (k = 0; k < wantP.length; k++) 
-             {
-                if(wantP[k].Name == gName)
-                {
-                  wantP.splice(k,1);
-                 }
-             }
-                 $('#'+curre).removeClass('pokSelctd');
-
-                  $("#" + curre + "wanbox").prop('checked',false)
-
+                for (k = 0; k < wantP.length; k++) 
+                  {
+                    if(wantP[k].Name == gName)
+                    {
+                       wantP.splice(k,1);
+                    }
+                  }
+                $('#'+curre).removeClass('pokSelctd');
+                $("#" + curre + "wanbox").prop('checked',false)
                }else { 
+                       wantP.push({"Dex" :gDex, "Name" : gName, "Type1" : gType1, "Type2" : gType2, "imaj" : gImj, "Region" :gReg, "shiny" :gShiny,"datecaught" : gdC, "notes": gNotes})
+                       $('#'+curre).addClass('pokSelctd');
+                       $("#"+curre +"wanbox").prop('checked',true)
+                     }
+  });
 
-                 wantP.push({"Dex" :gDex, "Name" : gName, "Type1" : gType1, "Type2" : gType2, "imaj" : gImj, "Region" :gReg, "shiny" :gShiny,"datecaught" : gdC, "notes": gNotes})
-
-                        $('#'+curre).addClass('pokSelctd');
-
-
-                        $("#"+curre +"wanbox").prop('checked',true)
-                }
-
-           });
+    $('#'+ data.PokemonTradingCenterGalar[a].Name + "gal").click(function(){
+    var gDex = data.PokemonTradingCenterGalar[a].Dex; var gName = data.PokemonTradingCenterGalar[a].Name;   var gReg = data.PokemonTradingCenterGalar[a].Region;   var gType1 = data.PokemonTradingCenterGalar[a].Type1;   var gType2 = data.PokemonTradingCenterGalar[a].Type2;   var gImj = data.PokemonTradingCenterGalar[a].imaj;   var gdC = data.PokemonTradingCenterGalar[a].datecaught;   var gNotes = data.PokemonTradingCenterGalar[a].notes;   var gPur = data.PokemonTradingCenterGalar[a].purified;   var gShiny = data.PokemonTradingCenterGalar[a].shiny; var curre = $(this).attr('id');
+                              
+              if($("#" + curre + "havbox").prop('checked'))
+              {
+                for (k = 0; k < haveP.length; k++) 
+                  {
+                    if(haveP[k].Name == gName)//this might be an issue with non-galar.nonalo
+                    {
+                       haveP.splice(k,1);
+                    }
+                  }
+                $('#'+curre).removeClass('pokSelctd');
+                $("#" + curre + "havbox").prop('checked',false)
+               }else { 
+                       haveP.push({"Dex" :gDex, "Name" : gName, "Type1" : gType1, "Type2" : gType2, "imaj" : gImj, "Region" :gReg, "shiny" :gShiny,"datecaught" : gdC, "notes": gNotes})
+                       $('#'+curre).addClass('pokSelctd');
+                       $("#"+curre +"havbox").prop('checked',true)
+                     }
+  });
 
                 };
-                
-
          })
   });
