7274573c197b2839668ec15436ca6b84e25111ae
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2021-04-08 10:05:40 -0400

    s

---
+++ beyond/scripts/ptcOneGather.js
@@ -0,0 +1,16 @@
+function getallPok(){
+        $.ajax({
+            method: "GET",
+            url: "https://aaronlilly.github.io/336/336ptc.json"
+               }).done(function(data) 
+                { 
+                    for(let i=0; i <  pokemonNameArray.length; i++) 
+                    {  
+                    	$("#pWant").append( '<div class="col-sm-3">' +'<div class="imjs"'+ 'id="'+ pokemonNameArray[i]
+                        + '">'+ '<figure>'+
+                        '<img src ="' + data.PokemonTradingCenter[i].imaj + '"style=\"width:96px;\">'
+                        + '<figcaption>' +data.PokemonTradingCenter[i].Name + 
+                        '</figcaption>'+'</figure>'+'</div></div>');
+                   	}
+                }
+            )};    
