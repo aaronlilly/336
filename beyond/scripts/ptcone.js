a3824067563980e7e7a7502c20004ae907f55f17
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2021-04-08 15:47:32 -0400

    looking good

--- beyond/scripts/ptcone.js
+++ beyond/scripts/ptcone.js
@@ -6,3 +6,68 @@ alert(text);
 }
 
 //End Motal Funct
+
+
+//toast menu
+$(document).ready(function() 
+{
+    	toastR();
+    	$('#menYou').click(function() {
+       		toastR();
+        });
+});
+//end toast menu
+
+
+//menyou
+$(document).ready(function () {
+    $('#hideAdd').click(function() {
+    $('#additionAl').css({
+        'height': '200px',
+        'width': '300',
+        'border': 'solid',
+        'border-color':'#17A2B8',
+        'border-radius':'5%',
+        'border-width':'thin',
+        'float':'left',
+        'margin-right':'2%',
+        'display':'none'
+    });
+
+      $('.addMenuUp').html("");
+      $('#selClick').css({'display':'none'});
+      });
+
+       $('#hideSubtract').click(function() {
+    $('#subtractionAl').css({
+        'height': '200px',
+        'width': '300',
+        'border': 'solid',
+        'border-color':'#17A2B8',
+        'border-radius':'5%',
+        'border-width':'thin',
+        'float':'right',
+        'margin-left':'2%',
+        'display':'none'
+    });
+
+      });
+       $('#selectAdd').click(function() {
+      $('.addMenuUp').html("");
+   $('#selClick').css({'display':'block'});
+$('#additionAl').css({
+'display':'block',
+'width':'315px',
+'border': 'solid',
+'border-color': '#17A2B8',
+'border-radius': '5%',
+'border-width':'thin',
+'display':'block',
+'float':'left',
+'margin-right':'2%',
+'height': 'auto'}
+);
+});
+
+});
+//menyou
---
+++ beyond/scripts/toastedOne.js
@@ -0,0 +1,71 @@
+function toastR(){
+toastr.options = {
+ "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "700",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+toastr["success"]('<span style="margin-right:20%; font-size:10px;">Menu </span> <div id="toastTop">User Options :</div><div id="myList"> My List - <span id ="myListBtns" style="margin-bottom:2%;">  <button type="button" class="btn btn-info" id="AddSaved">Add</button>  <button type="button" class="btn btn-danger" id="RemoveSaved">Remove</button> <button type="button" class="btn btn-warning" id="ViewSaved" style="margin-top:2%;">View</button>   <button type="button" class="btn btn-secondary" id="ShareSaved" style="margin-top:2%;">Share</button>  <button type="button" class="btn btn-primary" id="Advanced" style="margin-top:2%;">Advanced</button></span> </div> <hr style="border: 2px solid blue; border-radius: 5px;"> <div id="otherZ">Other Trainers -  <span id ="othersBtns"> <button type="button" class="btn btn-light" id="trainNam" style="margin-top:2%;" > Search</button> </div> <hr style="border: 2px solid white; border-radius: 5px;"><div> Save Data  -  <button type="button" id="dbSend" class="btn btn-dark">Save Lists</button></div> <div id ="endOfToast"></div>')
+ $('#clearly').click(function() 
+  {
+  toastr.remove();        
+  $("#menubox").prop('checked',false);
+  })
+      toastr.options.onHidden = function(){ $("#menubox").prop('checked',false)}; 
+ $('.toast-close-button').click(function() { $("#menubox").prop('checked',false);});
+    $('#AddSaved').click(function() {
+                       $('#subtractionAl').css({'display':'none'});
+                       $('.ViewAll').css({'display':'none'});
+
+    $('#additionAl').css({
+        'height': '200px',
+        'width': '320px',
+        'border': 'solid',
+        'border-color':'#17A2B8',
+        'border-radius':'5%',
+        'border-width':'thin',
+        'float':'left',
+        'margin-right':'2%',
+        'display':'block'
+    });
+      });
+     $('#RemoveSaved').click(function() {
+      $('#additionAl').css({'display':'none'});
+      $('.ViewAll').css({'display':'none'});
+                      $('#subtractionAl').css({
+        'height': '200px',
+        'width': '320px',
+        'border': 'solid',
+        'border-color':'#DC3545',
+        'border-radius':'5%',
+        'border-width':'thin',
+        'float':'right',
+        'margin-left':'2%',
+        'display':'block'
+    });
+      })
+     $('#ViewSaved').click(function() {
+                       $('#subtractionAl').css({'display':'none'});
+                       $('#additionAl').css({'display':'none'});
+                        $('.ViewAll').css({
+        'display':'block'
+    });
+
+      })
+     
+
+  }
+////end first toast -menu click
+
