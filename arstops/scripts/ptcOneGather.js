4af2dbe2ffc320afa35ed38773d0858b12fc15ce
Aaron Lilly <aaronlilly84@gmail.com>
2021-04-10 22:09:48 -0400

    c
    c

--- arstops/scripts/ptcOneGather.js
+++ arstops/scripts/ptcOneGather.js
@@ -10,16 +10,23 @@ $(document).ready(function () {
 function getallPok(){
         $.ajax({
             method: "GET",
-            url: "https://aaronlilly.github.io/336/336ptc.json"
+            url: "https://aaronlilly.github.io/336/arstops/scripts/Demo.json"
                }).done(function(data) 
                 { 
-                    for(let i=0; i <  pokemonNameArray.length; i++) 
+
+                  $("#pWantBody").append("<u> Stop Name</u> "+ " " + "<u> NearStreet</u> " + " " + "<u> Area</u> " +" <br>");
+
+                 
+                    for(let i=0; i < data.ArStops.length; i++) 
                     {  
-                    	$("#pWantBody").append( '<div class="col-sm-3">' +'<div class="imjs"'+ 'id="'+ pokemonNameArray[i]
-                        + '">'+ '<figure>'+
-                        '<img src ="' + data.PokemonTradingCenter[i].imaj + '"style=\"width:96px;\">'
-                        + '<figcaption>' +data.PokemonTradingCenter[i].Name + 
-                        '</figcaption>'+'</figure>'+'</div></div>');
+                    	$("#pWantBody").append(data.ArStops[i].Name +" " + data.ArStops[i].NearStreet + " " + data.ArStops[i].Area  );
                    	}
                 }
             )};    
+
+
+               //<div class="col-sm-3">' +'<div class="imjs"'+ 'id="'+ pokemonNameArray[i]
+                        // + '">'+ '<figure>'+
+                        // '<img src ="' + data.PokemonTradingCenter[i].imaj + '"style=\"width:96px;\">'
+                        // + '<figcaption>' +data.PokemonTradingCenter[i].Name + 
+                        // '</figcaption>'+'</figure>'+'</div></div>'
