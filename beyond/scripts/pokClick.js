715938f857241a357f598c7643218ecebe2d7c79
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2021-04-18 15:13:26 -0400

    g

---
+++ beyond/scripts/pokClick.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function () {
+       getallPok();
+       
+                      
+  });
+  
+
+
+
+
+function getallPok(){
+        $.ajax({
+            method: "GET",
+            url: "https://aaronlilly.github.io/336/336ptc.json"
+               }).done(function(data) 
+                { 
+                    for(let i=0; i <  pokemonNameArray.length; i++) 
+                    {  
+                      //pwant
+                    	$("#pWantBody").append( '<div class="col-sm-3">' +'<div class="imjs"'+ 'id="'+ pokemonNameArray[i]
+                        + '">'+ '<figure>'+
+                        '<img src ="' + data.PokemonTradingCenter[i].imaj + '"style=\"width:96px;\">'
+                        + '<figcaption>' +data.PokemonTradingCenter[i].Name + 
+                        '</figcaption>'+'</figure>'+'</div></div>');
+
+                        //chx box
+                      $("#xboxs").append('<input type="checkbox" id="' + pokemonNameArray[i] +'wantbox">');
+                      //end pwant
+
+                      //phave
+                      $("#pHaveBody").append( '<div class="col-sm-3">' +'<div class="imjs"'+ 'id="'+ pokemonNameArray[i]
+                        + "H" +'">'+ '<figure>'+
+                        '<img src ="' + data.PokemonTradingCenter[i].imaj  +'"style=\"width:96px;\">'
+                        + '<figcaption>' + data.PokemonTradingCenter[i].Name + 
+                        '</figcaption>'+'</figure>'+'</div></div>');
+
+                        //chx box
+                       $("#xboxs").append('<input type="checkbox" id="' + pokemonNameArray[i]+"H" +'havebox">');
+                      //end phave
+
+
+                   	}
+                }
+            )};    
